40d124ff78f962ba535ec6bb7cd3895f4cdfc82d
reserver7 <reserver7@gmail.com>
2024-05-17 15:52:19 +0900

    프로젝트 구성 및 환경설정

---
+++ src/components/shared/Layout.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import React from "react";
+import SEO from "./SEO";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <SEO
+        title="My Account"
+        description="내 자산 관리를 보다 쉽게!"
+        image=""
+      />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import "@/styles/globals.css";
+import Layout from "@/components/shared/Layout";
+import globalStyles from "@/styles/globalStyles";
+import { Global } from "@emotion/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />;
+    </Layout>
+  );
 }
